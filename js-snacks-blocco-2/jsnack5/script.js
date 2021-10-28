6249d1d98216930f2369c273d14c06daf59ae5b8
Tolihama <fra070@gmail.com>
2021-10-28 16:18:54 +0200

    Blocco 2 - Jsnack 5

---
+++ js-snacks-blocco-2/jsnack5/script.js
@@ -0,0 +1,61 @@
+/* JSnack 5 - Blocco 2
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro. */
+
+const randomElements = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'z'
+]
+
+const arrOne = [];
+const arrTwo = [];
+
+// Initial param
+const arrOneLength = 9;
+const arrTwoLength = 4;
+
+// Initial random generation arrays
+for (let i = 0; i < arrOneLength; i++) {
+    arrOne.push(randomElements[Math.floor(Math.random() * (randomElements.length - 1))]);
+}
+
+for (let i = 0; i < arrTwoLength; i++) {
+    arrTwo.push(randomElements[Math.floor(Math.random() * (randomElements.length - 1))]);
+}
+
+//
+console.log(arrOne);
+console.log(arrTwo);
+
+// Equalize array
+while (arrOne.length < arrTwo.length) {
+    arrOne.push(randomElements[Math.floor(Math.random() * (randomElements.length - 1))]);
+}
+while (arrOne.length > arrTwo.length) {
+    arrTwo.push(randomElements[Math.floor(Math.random() * (randomElements.length - 1))]);
+}
+
+// 
+console.log(arrOne);
+console.log(arrTwo);
+
